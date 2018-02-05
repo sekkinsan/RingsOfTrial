aae13a163f74ac84122f324abd4dee4a76d0e8d7
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-02-04 16:10:48 -0800

    made basic skeleton for login/user sign up, can't register properly

---
+++ mobile/platforms/android/app/src/main/assets/app/login.js
@@ -0,0 +1,14 @@
+var frameModule = require("ui/frame");
+
+exports.loaded = function() {
+    console.log("hello");
+};
+
+exports.signIn = function() {
+    alert("Signing in");
+};
+
+exports.register = function() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("register");
+};
